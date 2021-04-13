984b43fb57dfb86faecabac31acf871f55e05c1a
Paul Lotz <paul.lotz@gmx.at>
2021-04-13 20:10:24 +0200

    working ajax calls and changing design layout

--- backend/jsonClient/controller.js
+++ backend/jsonClient/controller.js
@@ -1,17 +1,39 @@
 // Loading JQuery
 $(document).ready(function () {
-    $("#searchResult").hide();
-    $("#userTable").hide();
+    loadAllTasks();
 
-    $("#btn_Search").click(function (e) {
-        loaddata($("#seachfield").val());
+    $("#btnSearchById").click(function () {
         $("#tbody").html("");
+        loadTaskById($("#txtSearchField").val());
+    });
+
+    $("#btnRefreshTasks").click(function (){
+        $("#tbody").html("");
+        loadAllTasks();
+    });
+
+    $("#btnSearchByName").click(function () {
+        $("#tbody").html("");
+        loadTaskByName($("#txtSearchField").val());
     });
 
     console.log("JS has been loaded");
 });
 
-function loaddata(searchterm) {
+function reloadEntries(response) {
+    $.each(response, function (i, item) {
+        var $tr = $("<tr>")
+            .append(
+                $("<td>").text(item.id),
+                $("<td>").text(item.name),
+                $("<td>").text(item.time),
+                $("<td>").text(item.location)
+            )
+            .appendTo("#tbody");
+    });
+}
+
+function loadTaskById(searchterm) {
     $.ajax({
         type: "GET",
         url: "../AppointmentFinder/backend/serviceHandler.php",
@@ -23,26 +45,73 @@ function loaddata(searchterm) {
         dataType: "json",
 
         success: function (response) {
-            console.log("Ajax Loaded");
-            $("#noOfentries").val(response.length);
+            console.log("Ajax Loaded: loadTaskById");
+            $("#noOfEntries").text("Open tasks: " + response.length);
             $("#searchResult").show(1000).delay(1000);
             $("#userTable").show(2000).delay(1000);
             $("#dynamic").load(
                 "../AppointmentFinder/backend/clientParts/part.html"
             );
 
-            $(function () {
-                $.each(response, function (i, item) {
-                    var $tr = $("<tr>")
-                        .append(
-                            $("<td>").text(item.id),
-                            $("<td>").text(item.name),
-                            $("<td>").text(item.time),
-                            $("<td>").text(item.location)
-                        )
-                        .appendTo("#tbody");
-                });
-            });
+            reloadEntries(response);
+        },
+
+        error: function (data) {
+            alert(data);
+            console.log("Ajax not loaded");
+        },
+    });
+}
+
+function loadTaskByName(searchterm) {
+    $.ajax({
+        type: "GET",
+        url: "../AppointmentFinder/backend/serviceHandler.php",
+        cache: false,
+        data: {
+            method: "queryAppointmentByName",
+            param: searchterm,
+        },
+        dataType: "json",
+
+        success: function (response) {
+            console.log("Ajax Loaded: loadTaskByName");
+            $("#noOfEntries").text("Open tasks: " + response.length);
+            $("#searchResult").show(1000).delay(1000);
+            $("#userTable").show(2000).delay(1000);
+            $("#dynamic").load(
+                "../AppointmentFinder/backend/clientParts/part.html"
+            );
+
+            reloadEntries(response);
+        },
+
+        error: function (data) {
+            console.log("Ajax not loaded");
+        },
+    });
+}
+
+function loadAllTasks() {
+    $.ajax({
+        type: "GET",
+        url: "../AppointmentFinder/backend/serviceHandler.php",
+        cache: false,
+        async: false,
+        data: {
+            method: "queryAppointments",
+        },
+        dataType: "json",
+
+        success: function (response) {
+            console.log(response);
+            console.log("Ajax Loaded: loadAllTasks");
+
+            $("#noOfEntries").text("Open tasks: " + response.length);
+            $("#searchResult").show(1000).delay(1000);
+            $("#userTable").show(2000).delay(1000);
+
+            reloadEntries(response);
         },
 
         error: function (data) {
